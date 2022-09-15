de78e4e439135e4ff58b54f55170634123b0af5f
Arun Kulshreshtha <kulshrax@gmail.com>
2022-09-15 13:38:33 +0000

    Add blobimport to mononoke.sh

--- mononoke.sh
+++ mononoke.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
-set -xeou pipefail
+set -euo pipefail
 
-repo="$HOME/eden"
+if [ "$#" -ne 1 ]; then
+    echo "usage: $0 path/to/repo"
+    exit 1
+fi
+repo="$1"
+
+if [ ! -d "$repo/.hg" ]; then
+  echo "$repo is not an hg repository"
+  exit 1
+fi
+
+eden_repo="$HOME/eden"
 bin="$HOME/edenscm/mononoke/bin"
 base="$HOME/mononoke"
 tmp=$(mktemp -d -p "$base")
 
 # Set up environment variables that would normally be set by the test harness.
-export TEST_FIXTURES="$repo/eden/mononoke/tests/integration"
+export TEST_FIXTURES="$eden_repo/eden/mononoke/tests/integration"
 export MONONOKE_SERVER="$bin/mononoke"
 export TESTTMP="$tmp"
 export TEST_CERTS="$TEST_FIXTURES/certs"
@@ -25,8 +36,8 @@ export URLENCODE="$tmp/urlencode.sh"
 export BLAME_VERSION=""
 export HG_SET_COMMITTER_EXTRA=""
 export SPARSE_PROFILES_LOCATION=""
-export HGRCPATH="$TESTTMP/.hgrc"
-export DUMMYSSH=""
+export HGRCPATH="$repo/.hg/hgrc"
+export DUMMYSSH="ssh"
 
 # The setup code in library.sh expects that $URLENCODE will contain a path to a
 # program with an `encode` subcommand that URL-encodes its argument. Since it
@@ -40,11 +51,20 @@ chmod +x "$URLENCODE"
 
 cd "$TEST_FIXTURES"
 
+set -x +u
+
 # shellcheck disable=SC1091
 . "$TEST_FIXTURES/library.sh"
 
+export REPOID=0
+export REPONAME=$(basename "$repo")
+export ENABLE=true
+
 setup_common_config
-REPOID=1 REPONAME=testrepo ENABLED=true setup_mononoke_config
-RUST_LOG=debug mononoke
+setup_mononoke_config
+
+RUST_LOG=debug start_and_wait_for_mononoke_server
+
+blobimport "$repo" "$repo-blob"
 
 tail -f "$TESTTMP/mononoke.out"
