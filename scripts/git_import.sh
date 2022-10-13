bc66d8562e4e45af3db0c411be7b3817f402e51e
Arun Kulshreshtha <kulshrax@gmail.com>
2022-10-13 21:02:46 +0000

    Uncomment gitimport

--- scripts/git_import.sh
+++ scripts/git_import.sh
@@ -29,8 +29,8 @@ ENABLED_DERIVED_DATA='["git_trees", "blame", "changeset_info",
   "deleted_manifest", "fastlog", "filenodes", "fsnodes", "unodes",
   "hgchangesets", "skeleton_manifests", "bssm"]' setup_common_config
 
-# gitimport --git-command-path=/usr/bin/git "$REPO" --derive-hg full-repo \
-# 2>&1 | tee "$TESTTMP/gitimport.out"
+gitimport --git-command-path=/usr/bin/git "$REPO" --derive-hg full-repo \
+  2>&1 | tee "$TESTTMP/gitimport.out"
 
 set -u
 
